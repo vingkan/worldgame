817d9867ac3335f8df051e5f48b93ca5331afed9
VINESH KANNAN <vingkan@gmail.com>
2015-06-29 20:10:34 -0500

    Added JQuery

--- mapengine/menu.js
+++ mapengine/menu.js
@@ -1,4 +1,4 @@
-var getPosition = function getPosition(event){
+function getPosition(event){
 	var canvas = document.getElementById('map');
 	var x = event.x;
 	var y = event.y;
@@ -7,12 +7,20 @@ var getPosition = function getPosition(event){
 	document.getElementById('nodeCoord-x').value = x;
 	document.getElementById('nodeCoord-y').value = y;
 	document.getElementById('nodeCoord').style.display = 'block';
-	canvas.removeEventListener('click', getPosition);
 }
 
 function newNode(){
-	var canvas = document.getElementById('map');
-	canvas.addEventListener('click', getPosition);
+	log("called");
+	$('map').one('mousedown', function(){
+		var canvas = document.getElementById('map');
+		var x = event.x;
+		var y = event.y;
+		x -= canvas.offsetLeft;
+		y -= canvas.offsetTop;
+		document.getElementById('nodeCoord-x').value = x;
+		document.getElementById('nodeCoord-y').value = y;
+		document.getElementById('nodeCoord').style.display = 'block';
+	});
 }
 
 function addNode(){
